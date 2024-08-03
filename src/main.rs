8a29c131a9a604a0b10d8a134b5591716a1b69ac
Vince <vhorvath2010@gmail.com>
2024-08-03 00:18:35 -0400

    add basic rectangle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+struct Rectangle {
+    height: u32,
+    width: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let rect1 = Rectangle {
+        height: 50,
+        width: 30,
+    };
+    println!("The area of rect1 is {} square units.", rect1.area());
+
+    let rect2 = Rectangle {
+        height: 40,
+        width: 10,
+    };
+
+    if rect1.can_hold(&rect2) {
+        println!("rect1 can hold rect 2!")
+    } else {
+        println!("rect1 cannot hold rect 2!")
+    }
 }
